e1dea2f5878aaa0d4b841b78e5538f4e07141102
Max Polzin <polzin.max@gmail.com>
2025-12-15 17:16:59 +0100

    Remove oldies.

--- oldies/ros-helix/ros_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} ros
-
-source /opt/ros/${ROS_DISTRO}/setup.bash
-spacenavd
-
-exec "$@"
