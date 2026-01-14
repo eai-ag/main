e9ca60a2cf07c8dcbbdbc3b1a28f969ba247cbb9
Max <m.polzin@embodiedai.ch>
2026-01-14 18:36:12 +0100

    Dev env.

---
+++ rpicam/camera.launch.py
@@ -0,0 +1,48 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+
+    # GStreamer pipeline with tee for dual output
+    # Matches docker-compose pipeline with videocrop settings
+    # Note: gscam automatically adds the appsink, so we define everything before it
+    gst_pipeline = (
+        'libcamerasrc ! '
+        'video/x-raw,width=1280,height=720,framerate=30/1 ! '
+        'videocrop left=270 right=290 ! '
+        'videoconvert ! '
+        'tee name=t '
+        't. ! queue ! jpegenc ! multipartmux ! tcpserversink host=0.0.0.0 port=5000'
+        't. ! queue ! videoconvert ! video/x-raw,format=RGB '
+    )
+
+
+    # gst_pipeline = "v4l2src ! video/x-raw,width=1280,height=720 ! videocrop left=270 right=290 ! tee name=t t. ! queue ! jpegenc ! multipartmux ! tcpserversink host=0.0.0.0 port=5000 t. ! queue ! videoconvert ! video/x-raw,format=RGB"
+    
+asdf
+    gscam_node = Node(
+        package='gscam',
+        executable='gscam_node',
+        name='gscam_driver',
+        parameters=[{
+            'gscam_config': gst_pipeline,
+            'camera_name': 'camera',
+            'frame_id': 'camera',
+            'camera_info_url': '',
+            'use_gst_timestamps': False,
+            'image_encoding': 'rgb8',
+            'sync_sink': True,
+        }],
+        remappings=[
+            ('camera/image_raw', 'image_raw'),
+            ('camera/camera_info', 'camera_info'),
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gscam_node,
+    ])
--- rpicam/run.sh
+++ rpicam/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker compose run --remove-orphans -it rpicam bash
+docker compose run --remove-orphans -it  \
+-v $(pwd)/camera.launch.py:/root/ros2_humble/install/gscam/share/gscam/examples/camera.launch.py \
+rpicam bash
 
